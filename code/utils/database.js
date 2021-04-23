94f843e84b8360ed5f329e5e70e5d35ac2936c33
koushi007 <saikoushik.guntakanti@gmail.com>
2021-04-23 12:37:47 +0530

    added Add product, View product, few messages

--- code/utils/database.js
+++ code/utils/database.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres', //your postgres username
     host: '127.0.0.1',
     database: 'emercado', //your local database 
-    password: 'qwerty', //your postgres user password
+    password: 'siva@1970', //your postgres user password
     port: 5432, //your postgres running port
 });
 
